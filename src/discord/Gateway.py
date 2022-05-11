f0340c1eba31b332b9aa83e01db8f5756ae9d55c
didlly <94558954+didlly@users.noreply.github.com>
2022-05-11 16:39:06 +0100

    Fix bugs.

--- src/discord/Gateway.py
+++ src/discord/Gateway.py
@@ -199,12 +199,15 @@ def event_handler(Client, ws, event: dict) -> None:
                 gateway(Client)
             elif event["t"] == "MESSAGE_CREATE":
                 length = len(Client.Repository.config["settings"]["prefix"])
-                
+                print(length)
+                print(type(Client.Repository.config["settings"]["prefix"]))
+                print(Client.Repository.config["settings"]["prefix"])
+                print(event["d"]["content"][:length])
                 if (
                     event["d"]["content"][:length] == Client.Repository.config["settings"]["prefix"]
                     and len(event["d"]["content"]) > length+2
                 ):
-
+                    
                     if event["d"]["author"]["id"] in Client.Repository.controllers[
                         "controllers"
                     ] and (
