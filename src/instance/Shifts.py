5ee71b4c2e53fc96431af80457f88c7ba23b14fb
didlly <94558954+didlly@users.noreply.github.com>
2022-05-12 17:15:16 +0100

    Oops?

--- src/instance/Shifts.py
+++ src/instance/Shifts.py
@@ -1,42 +1,10 @@
 from instance.Client import Instance
-from database.Handler import Database
 from datetime import datetime, timedelta
-from json import dumps
 from utils.Shared import data
 from time import sleep
 from random import uniform
 
-def shifts(Client: Instance) -> None:
-    """
-    while True:
-        if (
-            not data[Client.username]
-            and (
-                datetime.strptime(datetime.now().strftime("%x-%X"), "%x-%X")
-                - datetime.strptime(Repository.database["shifts"]["passive"], "%x-%X")
-            ).total_seconds()
-            >= Repository.config["shifts"]["passive"]
-        ):
-            data[Client.username] = True
-            Repository.database["shifts"]["passive"] = datetime.now().strftime("%x-%X")
-            Repository.database["shifts"]["active"] = datetime.now().strftime("%x-%X")
-            Repository.database_write()
-            Client.log("DEBUG", "Beginning active phase.")
-        elif (
-            data[Client.username]
-            and (
-                datetime.strptime(datetime.now().strftime("%x-%X"), "%x-%X")
-                - datetime.strptime(Repository.database["shifts"]["active"], "%x-%X")
-            ).total_seconds()
-            >= Repository.config["shifts"]["active"]
-        ):
-            data[Client.username] = False
-            Repository.database["shifts"]["active"] = datetime.now().strftime("%x-%X")
-            Repository.database["shifts"]["passive"] = datetime.now().strftime("%x-%X")
-            Repository.database_write()
-            Client.log("DEBUG", "Beginning passive phase.")
-    """
-        
+def shifts(Client: Instance) -> None:       
     index = Client.Repository.database["shifts"]["shift"]
        
     index = 1 if index not in Client.Repository.config["shifts"].keys() else -index+1
