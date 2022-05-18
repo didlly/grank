be67d5c1b1af912144965900585635e5dc713fc2
didlly <deepinord@gmail.com>
2022-05-18 14:56:38 +0000

    Fix bugs.

--- src/discord/GuildId.py
+++ src/discord/GuildId.py
@@ -4,37 +4,17 @@ from requests import get
 
 
 def guild_id(Client):
-    """Gets the `guild_id` of the channel specified by the `channel_id` in the `Client` argument.
-
-    Args:
-            Client (class): The Client class for the account.
-
-    Returns:
-            None
-    """
-
     req = get(
-        f"https://discord.com/api/v10/channels/{Client.channel_id}/messages",
+        f"https://discord.com/api/v10/channels/{Client.channel_id}",
         headers={"authorization": Client.token},
     )
-
+  
     if req.status_code == 404:
         return False
-
+    
     response = loads(req.content.decode())
-
-    if len(response) != 0:
-        for latest_message in response:
-            if "message_reference" in latest_message.keys():
-                return latest_message["message_reference"]["guild_id"]
-
-    Client.send_message("pls beg")
-
-    latest_message = loads(
-        get(
-            f"https://discord.com/api/v10/channels/{Client.channel_id}/messages",
-            headers={"authorization": Client.token},
-        ).content.decode()
-    )[0]
-
-    return latest_message["message_reference"]["guild_id"]
+    
+    if reponse["type"] != 0:
+        return False
+    
+    return response["guild_id"]
